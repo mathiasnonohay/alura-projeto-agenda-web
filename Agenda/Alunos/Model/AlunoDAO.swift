2fca5bffe254e65b0d71655bf25e774e21f715df
Mathias Nonohay <mathias12300@gmail.com>
2020-12-28 16:45:05 -0300

    coodigo limpo e mostrando alunos na agenda salvos no servidor

--- Agenda/Alunos/Model/AlunoDAO.swift
+++ Agenda/Alunos/Model/AlunoDAO.swift
@@ -11,9 +11,28 @@ import CoreData
 
 class AlunoDAO: NSObject {
     
+    var gerenciadorDeResultados:NSFetchedResultsController<Aluno>?
     var contexto:NSManagedObjectContext {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         return appDelegate.persistentContainer.viewContext
+    }
+    
+    func recuperaAlunos() -> Array<Aluno> {
+        let pesquisaAluno:NSFetchRequest<Aluno> = Aluno.fetchRequest()
+        let ordenaPorNome = NSSortDescriptor(key: "nome", ascending: true)
+        pesquisaAluno.sortDescriptors = [ordenaPorNome]
+        
+        gerenciadorDeResultados = NSFetchedResultsController(fetchRequest: pesquisaAluno, managedObjectContext: contexto, sectionNameKeyPath: nil, cacheName: nil)
+        
+        do {
+            try gerenciadorDeResultados?.performFetch()
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        guard let listaDeAlunos = gerenciadorDeResultados?.fetchedObjects else { return [] }
+
+        return listaDeAlunos
     }
     
     func salvaAluno(dicionarioDeAluno:Dictionary<String, Any>) {
@@ -22,7 +41,16 @@ class AlunoDAO: NSObject {
         aluno.endereco = dicionarioDeAluno["endereco"] as? String
         aluno.telefone = dicionarioDeAluno["telefone"] as? String
         aluno.site = dicionarioDeAluno["site"] as? String
-        aluno.nota = (dicionarioDeAluno["nota"] as! NSString).doubleValue
+        
+        guard let nota = dicionarioDeAluno["nota"] else { return }
+        
+        if (nota is String) {
+            
+            aluno.nota = (dicionarioDeAluno["nota"] as! NSString).doubleValue
+        } else {
+            let conversaoDeNota = String(describing: nota)
+            aluno.nota = (conversaoDeNota as NSString).doubleValue
+        }
         atualizaContexto()
     }
     
--- Agenda/Alunos/Model/Repositorio.swift
+++ Agenda/Alunos/Model/Repositorio.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class Repositorio: NSObject {
 
+    func recuperaAlunos(completion:@escaping(_ listaDeAlunos:Array<Aluno>) -> Void) {
+        var alunos = AlunoDAO().recuperaAlunos()
+        if alunos.count == 0 {
+            AlunoAPI().recuperaAlunos {
+                alunos = AlunoDAO().recuperaAlunos()
+                completion(alunos)
+            }
+        } else {
+            completion(alunos)
+        }
+    }
+    
     func salvaAluno(aluno:Dictionary<String, String>){
         AlunoAPI().salvaAlunosNoServidor(parametros: [aluno])
         AlunoDAO().salvaAluno(dicionarioDeAluno: aluno)
