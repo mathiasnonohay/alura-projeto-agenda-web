a59cda0380a503fbe3b603f2dd335b39d8a08daa
Mathias Nonohay <mathias12300@gmail.com>
2020-12-29 09:59:02 -0300

    crud completo e feito sincronização com o servidor

--- Agenda/Alunos/Model/AlunoDAO.swift
+++ Agenda/Alunos/Model/AlunoDAO.swift
@@ -68,6 +68,11 @@ class AlunoDAO: NSObject {
         atualizaContexto()
     }
     
+    func deletaAluno(aluno:Aluno) {
+        contexto.delete(aluno)
+        atualizaContexto()
+    }
+    
     func atualizaContexto() {
         do {
             try contexto.save()
